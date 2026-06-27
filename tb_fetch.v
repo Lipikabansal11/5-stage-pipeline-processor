5272c912eeaa42f3fdb18dc287906f8dd28fa6d9
Lipika <lipikabansal11@gmail.com>
2026-06-27 16:39:16 +0530

    Add testbench for fetch_cycle module

---
+++ tb_fetch.v
@@ -0,0 +1,47 @@
+`include "PC.v"
+`include "PC_Adder.v"
+`include "Mux.v"
+`include "Instruction_Memory.v"
+
+module tb;
+
+    // Declare I/O
+    reg clk = 1, rst, PCSrcE;
+    reg [31:0] PCTargetE;
+    wire [31:0] InstrD, PCD, PCPlus4D;
+
+    // Declare the design under test
+    fetch_cycle dut (
+        .clk(clk),
+        .rst(rst),
+        .PCSrcE(PCSrcE),
+        .PCTargetE(PCTargetE),
+        .InstrD(InstrD),
+        .PCD(PCD),
+        .PCPlus4D(PCPlus4D)
+    );
+
+    // Generation of clock
+    always begin
+        clk = ~clk;
+        #50;
+    end
+
+    // Provide the Stimulus
+    initial begin
+        rst <= 1'b0;
+        #200;
+        rst <= 1'b1;
+        PCSrcE <= 1'b0;
+        PCTargetE <= 32'h00000000;
+        #500;
+        $finish;
+    end
+
+    // Generation of VCD File
+    initial begin
+        $dumpfile("dump.vcd");
+        $dumpvars(0);
+    end
+
+endmodule
